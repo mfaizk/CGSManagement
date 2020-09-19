e981dd80cde8df85ae93806de2d81ee28a5886eb
Ujjawalmaurya <chocolateboyz0011@gmail.com>
2020-09-20 01:17:33 +0530

    some colors added in Constants

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const themeBlueColor = Color(0xFF2032EB);
+
+const themeBlueLightColor = Color(0xFF2D40E6);
+
+const themeLightGrayColor = Color(0xFFF2F5F8);
+
+const themeRedColor = Color(0xFFEE1A43);
+
+const themeOrangeColor = Color(0xFFFF4D13);
